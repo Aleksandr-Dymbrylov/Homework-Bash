6239e9866803f1821572f734f03c4d48c8d6b2da
Aleksandr-Dymbrylov <168312853+Aleksandr-Dymbrylov@users.noreply.github.com>
2024-05-18 00:26:20 -0500

    Update kaizen.sh

--- Homework8/kaizen.sh
+++ Homework8/kaizen.sh
@@ -5,3 +5,23 @@ touch kaizen/kaizen
 touch hello/kaizen
 touch world/kaizen
 touch alex/kaizen
+chmod kaizen/kaizen
+chmod hello/kaizen
+chmod world/kaizen
+chmod alex/kaizen
+useradd tim
+useradd brad
+useradd eric
+useradd ann
+useradd jenny
+groupadd devops
+groupadd dev
+groupadd qa
+usermod -aG devops tim
+usermod -aG devops brad
+usermod -aG devops eric
+usermod -aG qa ann
+usermod -aG dev jenny
+yum install -y wget tree httpd
+systemctl start httpd
+systemctl enable httpd
